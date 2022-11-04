f12b2629e0f05bc57d0342e3122e72083cbf4bca
Mustafoyev <mmustafoyev9@gmail.com>
2022-11-04 20:13:02 +0500

    commit message

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,14 @@
 package com.epam.demo;
 
 import java.util.List;
+import java.util.function.Predicate;
+
+import com.epam.utils.StringUtils;
+import org.apache.commons.lang3.math.*;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
+        return args.stream().filter(Predicate.not(StringUtils::isPositiveNumber)).findFirst().isEmpty();
        //magic happens here
     }
 }
